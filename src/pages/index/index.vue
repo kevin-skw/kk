4ef70ff632a562871f51d4d2cb57bef2729930e6
kevin <1594799975@qq.com>
2019-10-13 16:47:24 +0800

    ss

---
+++ src/pages/index/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="seach"> 
+       <input type="text" :class="{feach:feach}" @click="seach()">
+       <p class="cancet" @click="quxiao()" >取消</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      feach :false
+    }
+  },
+
+  methods: {
+    seach(){
+      console.log(110)
+      this.feach = true;
+    },
+    quxiao(){
+      console.log(220)
+      this.feach = false;
+    }
+  }
+}
+</script>
+
+<style scoped  >
+
+.seach{
+  height: 60rpx;
+  background:#ea4451;
+ 
+}
+.feach{
+  display: flex;
+  height: 60rpx;
+  background: pink;
+  /*position: absolute;*/
+  /*margin: 0 80rpx 0 0;*/
+}
+input{
+  flex: 1;
+  width: 80%;
+  background: #ffffff;
+  border-radius: 10rpx;
+}
+.cancet{
+  width: 80rpx;
+  height: 60rpx;
+  line-height: 60rpx;
+  font-size: 30rpx;
+  /*position: relative;
+  left: 600rpx;*/
+  display: none;
+}
+</style>
---
+++ src/pages/logs/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>啦啦</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped >
+</style>
